e8b70368a341378a1aad2697d62a77a2b0472793
Abbie <abbigail.moulin@gmail.com>
2023-06-13 09:58:57 -0500

    Initial Commit

---
+++ SocialSerenity/SocialSerenityApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialSerenityApp.swift
+//  SocialSerenity
+//
+//  Created by Scholar on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct SocialSerenityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
